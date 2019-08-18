4dc457379c4cae77dc4ae7db93e8cd166f3f178c
Alexandr <muxachito@mail.ru>
2019-08-18 22:56:22 +0300

    3 commit

--- script.js
+++ script.js
@@ -5,31 +5,30 @@
 
  function start(){
 
- 	monye = prompt("Ваш бюджет?");
+ 	monye = prompt("Ваш бюджет?","");
 // проверка на число, пусые строки и кэнсел
  	while(isNaN(monye) || monye == " " || monye == "" || monye == null)
- 		monye = prompt("Ваш бюджет?");
+ 		monye = prompt("Ваш бюджет?","");
 
- 	name = prompt("Название магазина").toUpperCase();
+ 	name = prompt("Название магазина","").toUpperCase();
  	time = 21;
  }
 
- start();
+ //start();
 
  let mainList = {
- 	budjet: monye,
+ 	budget: monye,
  	shopName: name,
  	shopGoods: [],
  	employers: {},
  	open: false,
- 	discount: false
- }
-
- function chooseGoods(){
+ 	discount: false,
+ 	shopItems: [],
+ 	chooseGoods: function chooseGoods(){
 
  		for(let i=0; i<5; i++){
 
- 			let good = prompt("Выберите товар");
+ 			let good = prompt("Выберите товар","");
  			if(typeof(good) === "string" && typeof(good) != null && good!="" && good!=" "){
  				console.log("Все верно ", i);
  				mainList.shopGoods[i] = good;
@@ -39,16 +38,61 @@
  					console.log("else " + i);
  				 }
  		}
- }
-
- chooseGoods();
-
- function workTime(time){
+ },
+ workTime: function workTime(time){
 
  		if(time < 8)console.log("Еще слишком рано");
- 		else if(time >=8 && time <20)console.log("Время работать");
+ 		else if(time >=8 && time <20){console.log("Время работать"); mainList.open = true; console.log(mainList.open);}
  		else if(time < 24)console.log("Уже слишком поздно");
- 		else console.log("В сутках 24 часа!")	;
+ 		else console.log("В сутках 24 часа!");
+ },
+
+ dayBudget: function dayBudget(){
+ 		alert("Ежедневный бюджет " + mainList.budget/30);
+ },
+
+ makeDiscount: function makeDiscount(){
+ 	if(mainList.discount == true) mainList.price = price*0.8
+ },
+
+ hireEmployers: function hireEmployers(){
+ 	for(var i = 0; i <= 4; i++){
+ 		mainList.employers[i] = prompt("Введите имя сотрудника");
+ 	}
+ },
+
+ chooseShopItems: function chooseShopItems(){
+ 	var x = false;
+ 	var arr;
+ 	while(x == false){
+ 	 let items = prompt("Перечислите товары через запятую", "")
+ 	 if(items == null){alert("введите правильно");continue;}
+ 	 var arr = items.split(",");
+ 	 for(var i = 0; i < arr.length; i++) {
+
+  		if(typeof(arr[i]) === "string" && typeof(arr[i]) != null && arr[i]!="" && arr[i]!=" ") x = true;
+  		else {x = false;alert("введите снова");break }
+		};
+
+	}
+	 mainList.shopItems = arr;	
+ 	 mainList.shopItems.sort();
+
+ 	 var str = "";
+ 	 mainList.shopItems.forEach(function(item, i, arr){
+ 	 			str = str + (i + 1) + ". " + item + ", ";
+ 	 })
+  	 var str1 = "В нашем магазине вы можете купить " + str;
+  	 console.log(str1);
+ 	},
+
  }
 
-workTime(5);
+ 
+
+ //mainList.chooseGoods();
+ //mainList.workTime(9);
+ console.log(mainList);
+
+
+
